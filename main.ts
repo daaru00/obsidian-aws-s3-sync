6368717f93c5caad9b32e42f16baadd311197dbf
Fabio Gollinucci <fbgolly@gmail.com>
2021-03-31 21:34:31 +0200

    Refactoring classes

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
 import * as os from 'os'
 import * as path from 'path'
-import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian'
-import { AwsCredentials, AwsProfile, REGIONS } from './lib/aws'
+import { Notice, Plugin } from 'obsidian'
+import { AwsCredentials, AwsProfile } from './lib/aws'
 import { FileManager, SyncStat, SyncDirection } from './lib/filemanager'
-
-const MESSAGE_PREFIX = 'AWS S3: '
+import { AwsS3SyncSettingTab, AwsSyncPluginSettings, DEFAULT_SETTINGS } from 'settings'
 
 enum PluginState {
 	LOADING,
@@ -13,36 +12,6 @@ enum PluginState {
 	SYNCHING,
 	CHECKING,
 	ERROR
-}
-
-interface AwsSyncPluginSettings {
-	profile: string;
-	region: string;
-	bucketName: string;
-	bucketPathPrefix: string;
-	localFileProtection: boolean;
-	syncDirection: SyncDirection;
-	enableStatusBar: boolean;
-	enableNotifications: boolean;
-	enableAutoSync: boolean;
-	autoSyncDebounce: number;
-  enableAutoPull: boolean;
-  autoPullInterval: number;
-}
-
-const DEFAULT_SETTINGS: AwsSyncPluginSettings = {
-	profile: 'default',
-	region: 'us-east-1',
-	bucketName: '',
-	bucketPathPrefix: '/%VAULT_NAME%/',
-	localFileProtection: true,
-	syncDirection: SyncDirection.FROM_LOCAL,
-	enableStatusBar: true,
-	enableNotifications: true,
-	enableAutoSync: false,
-	autoSyncDebounce: 2,
-  enableAutoPull: false,
-  autoPullInterval: 300,
 }
 
 export default class AwsSyncPlugin extends Plugin {
@@ -60,7 +29,7 @@ export default class AwsSyncPlugin extends Plugin {
 		await this.awsCredentials.loadProfiles()
 
 		await this.loadSettings();
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new AwsS3SyncSettingTab(this.app, this));
 
 		this.statusBarItem = this.addStatusBarItem()
 		this.setState(PluginState.LOADING)
@@ -130,14 +99,14 @@ export default class AwsSyncPlugin extends Plugin {
 		if (!this.settings.enableStatusBar) {
 			return
 		}
-		this.statusBarItem.innerHTML = MESSAGE_PREFIX+' '+msg;
+		this.statusBarItem.innerHTML = msg + ' S3 bucket';
 	}
 
 	sendNotification(msg: string): void {
 		if (!this.settings.enableNotifications) {
 			return
 		}
-		new Notice(MESSAGE_PREFIX+' '+msg)
+		new Notice('S3 bucket sync '+msg)
 	}
 
 	async onLocalFileChanged() {
@@ -193,18 +162,24 @@ export default class AwsSyncPlugin extends Plugin {
 		}
 
 		if (this.settings.enableAutoSync) { 
-			this.setStatusBarText('waiting..');
+			this.setStatusBarText('...');
 			return
 		}
 
 		if (this.fileManager.isInSync()) {
-			this.setStatusBarText('in sync');
+			this.setStatusBarText('0');
 		} else {
-			this.setStatusBarText(
-				`&#8593; ${this.syncStatus.filesToUpload.length} `+
-				`&#8595; ${this.syncStatus.filesToDownload.length} ` +
-				`&#215; ${this.syncStatus.filesToDelete.length} `
-			);
+			let msgs = []
+			if (this.syncStatus.filesToUpload.length > 0) {
+				msgs.push(`${this.syncStatus.filesToUpload.length} &#8593;`)
+			}
+			if (this.syncStatus.filesToDownload.length > 0) {
+				msgs.push(`${this.syncStatus.filesToDownload.length} &#8595;`)
+			}
+			if (this.syncStatus.filesToDelete.length > 0) {
+				msgs.push(`${this.syncStatus.filesToDelete.length} &#215;`)
+			}
+			this.setStatusBarText(msgs.join(' '));
 		}
 	}
 
@@ -226,15 +201,15 @@ export default class AwsSyncPlugin extends Plugin {
 				}
 				break;
 			case PluginState.TESTING:
-				this.setStatusBarText('testing..');
+				this.setStatusBarText('...');
 				this.sendNotification('testing..');
 				break;
 			case PluginState.SYNCHING:
-				this.setStatusBarText('running..');
+				this.setStatusBarText('...');
 				this.sendNotification('running..');
 				break;
 			case PluginState.CHECKING:
-				this.setStatusBarText('checking..');
+				this.setStatusBarText('...');
 				break;
 			case PluginState.ERROR:
 				this.setStatusBarText('error');
@@ -407,179 +382,4 @@ export default class AwsSyncPlugin extends Plugin {
     this.pullInterval = window.setInterval(this.runRemotePull.bind(this), this.settings.autoPullInterval * 1000)
     this.registerInterval(this.pullInterval)
   }
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: AwsSyncPlugin;
-
-	constructor(app: App, plugin: AwsSyncPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	async display(): Promise<void> {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		const profiles = await this.plugin.awsCredentials.loadProfiles()
-		if (profiles.length > 0) {
-			new Setting(containerEl)
-				.setName('AWS Profile')
-				.setDesc('The name AWS profile name configured in credentials file')
-				.addDropdown(dropdown => dropdown
-					.addOptions(profiles.reduce((acc: any, profile: AwsProfile) => {
-						acc[profile.name] = profile.name
-						return acc
-					}, {}))
-					.setValue(this.plugin.settings.profile)
-					.onChange(async (value) => {
-						this.plugin.settings.profile = value;
-						await this.plugin.saveSettings();
-					}));
-		} else {
-			containerEl.createEl('p', {text: 'Cloud not find any AWS profiles!', cls: ['setting-item', 'aws-s3-no-profile']});
-		}
-
-    new Setting(containerEl)
-      .setName('AWS Region')
-      .setDesc('The region where S3 bucket was created')
-      .addDropdown(dropdown => dropdown
-        .addOptions(REGIONS)
-        .setValue(this.plugin.settings.region)
-        .onChange(async (value) => {
-          this.plugin.settings.region = value;
-          await this.plugin.saveSettings();
-        }));
-
-		new Setting(containerEl)
-			.setName('AWS S3 Bucket Name')
-			.setDesc('The name of the AWS S3 bucket')
-			.addText(text => text
-				.setValue(this.plugin.settings.bucketName)
-				.setPlaceholder("REQUIRED")
-				.onChange(async (value) => {
-					this.plugin.settings.bucketName = value;
-					await this.plugin.saveSettings();
-				}));
-		
-		new Setting(containerEl)
-			.setName('AWS S3 Objects Path Prefix')
-			.setDesc('The prefix of uploaded objects to the AWS S3 bucket')
-			.addText(text => text
-				.setPlaceholder('use %VAULT_NAME% as placeholder')
-				.setValue(this.plugin.settings.bucketPathPrefix)
-				.onChange(async (value) => {
-					this.plugin.settings.bucketPathPrefix = value;
-					await this.plugin.saveSettings();
-				}));
-
-		new Setting(containerEl)
-			.setName('Test configurations')
-			.setDesc('Will check if credentials are valid and S3 bucket exist')
-			.addButton(button => button
-				.setButtonText("Test")
-				.onClick(async () => {
-					await this.plugin.runTest()
-				}));
-
-		new Setting(containerEl)
-			.setName('Local file protection')
-			.setDesc('Protect local files from deletion')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.localFileProtection)
-				.onChange(async (value) => {
-					this.plugin.settings.localFileProtection = value;
-					await this.plugin.saveSettings();
-				}));
-
-		new Setting(containerEl)
-			.setName('Synch direction')
-			.setDesc('Non existing files in destination will be deleted if not found in source')
-			.addDropdown(dropdown => dropdown
-        .addOption(SyncDirection.FROM_LOCAL.toString(), "from local to remote")
-        .addOption(SyncDirection.FROM_REMOTE.toString(), "from remote to local")
-				.setValue(this.plugin.settings.syncDirection.toString())
-				.onChange(async (value) => {
-          this.plugin.settings.syncDirection = parseInt(value)
-					await this.plugin.saveSettings();
-				}));
-
-		new Setting(containerEl)
-			.setName('Enable status bar')
-			.setDesc('Show the synchronization status application bottom bar')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.enableStatusBar)
-				.onChange(async (value) => {
-					this.plugin.settings.enableStatusBar = value;
-					await this.plugin.saveSettings();
-				}));
-
-		new Setting(containerEl)
-			.setName('Enable notifications')
-			.setDesc('Show notifications whe synchronization status change')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.enableNotifications)
-				.onChange(async (value) => {
-					this.plugin.settings.enableNotifications = value;
-					await this.plugin.saveSettings();
-				}));
-
-		new Setting(containerEl)
-			.setName('Enable automatic synchronization')
-			.setDesc('Automatically synchronize changes')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.enableAutoSync)
-				.onChange(async (value) => {
-					this.plugin.settings.enableAutoSync = value;
-					await this.plugin.saveSettings();
-				}));
-
-		new Setting(containerEl)
-			.setName('Automatic synchronization debounce')
-			.setDesc('Delay the synchronization respect vault changes')
-			.addDropdown(dropdown => dropdown
-				.addOptions({
-					'0': "disabled",
-					'2': "2 seconds",
-					'5': "5 seconds",
-					'10': "10 seconds",
-					'30': "30 seconds",
-					'60': "1 minute",
-					'300': "5 minutes",
-				})
-				.setValue(this.plugin.settings.autoSyncDebounce.toString())
-				.onChange(async (value) => {
-					this.plugin.settings.autoSyncDebounce = parseInt(value);
-					await this.plugin.saveSettings();
-				}));
-
-    new Setting(containerEl)
-			.setName('Enable automatic pull')
-			.setDesc('Automatically pull changes from S3 bucket')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.enableAutoPull)
-				.onChange(async (value) => {
-					this.plugin.settings.enableAutoPull = value;
-					await this.plugin.saveSettings();
-				}));
-
-		new Setting(containerEl)
-			.setName('Automatic pull interval')
-			.setDesc('Interval between S3 bucket changes checks')
-			.addDropdown(dropdown => dropdown
-				.addOptions({
-					'10': "10 seconds",
-					'60': "1 minute",
-					'300': "5 minutes",
-					'600': "10 minutes",
-					'1800': "30 minutes",
-				})
-				.setValue(this.plugin.settings.autoPullInterval.toString())
-				.onChange(async (value) => {
-					this.plugin.settings.autoPullInterval = parseInt(value);
-					await this.plugin.saveSettings();
-				}));
-
-	}
 }
---
+++ settings.ts
@@ -0,0 +1,209 @@
+import { App, PluginSettingTab, Setting } from 'obsidian'
+import AwsSyncPlugin from 'main';
+import { AwsProfile, REGIONS } from './lib/aws'
+import { SyncDirection } from './lib/filemanager'
+
+export interface AwsSyncPluginSettings {
+	profile: string;
+	region: string;
+	bucketName: string;
+	bucketPathPrefix: string;
+	localFileProtection: boolean;
+	syncDirection: SyncDirection;
+	enableStatusBar: boolean;
+	enableNotifications: boolean;
+	enableAutoSync: boolean;
+	autoSyncDebounce: number;
+  enableAutoPull: boolean;
+  autoPullInterval: number;
+}
+
+export const DEFAULT_SETTINGS: AwsSyncPluginSettings = {
+	profile: 'default',
+	region: 'us-east-1',
+	bucketName: '',
+	bucketPathPrefix: '/%VAULT_NAME%/',
+	localFileProtection: true,
+	syncDirection: SyncDirection.FROM_LOCAL,
+	enableStatusBar: true,
+	enableNotifications: true,
+	enableAutoSync: false,
+	autoSyncDebounce: 2,
+  enableAutoPull: false,
+  autoPullInterval: 300,
+}
+
+export class AwsS3SyncSettingTab extends PluginSettingTab {
+	plugin: AwsSyncPlugin;
+
+	constructor(app: App, plugin: AwsSyncPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	async display(): Promise<void> {
+		let {containerEl} = this;
+
+		containerEl.empty();
+
+		const profiles = await this.plugin.awsCredentials.loadProfiles()
+		if (profiles.length > 0) {
+			new Setting(containerEl)
+				.setName('AWS Profile')
+				.setDesc('The name AWS profile name configured in credentials file')
+				.addDropdown(dropdown => dropdown
+					.addOptions(profiles.reduce((acc: any, profile: AwsProfile) => {
+						acc[profile.name] = profile.name
+						return acc
+					}, {}))
+					.setValue(this.plugin.settings.profile)
+					.onChange(async (value) => {
+						this.plugin.settings.profile = value;
+						await this.plugin.saveSettings();
+					}));
+		} else {
+			containerEl.createEl('p', {text: 'Cloud not find any AWS profiles!', cls: ['setting-item', 'aws-s3-no-profile']});
+		}
+
+    new Setting(containerEl)
+      .setName('AWS Region')
+      .setDesc('The region where S3 bucket was created')
+      .addDropdown(dropdown => dropdown
+        .addOptions(REGIONS)
+        .setValue(this.plugin.settings.region)
+        .onChange(async (value) => {
+          this.plugin.settings.region = value;
+          await this.plugin.saveSettings();
+        }));
+
+		new Setting(containerEl)
+			.setName('AWS S3 Bucket Name')
+			.setDesc('The name of the AWS S3 bucket')
+			.addText(text => text
+				.setValue(this.plugin.settings.bucketName)
+				.setPlaceholder("REQUIRED")
+				.onChange(async (value) => {
+					this.plugin.settings.bucketName = value;
+					await this.plugin.saveSettings();
+				}));
+		
+		new Setting(containerEl)
+			.setName('AWS S3 Objects Path Prefix')
+			.setDesc('The prefix of uploaded objects to the AWS S3 bucket')
+			.addText(text => text
+				.setPlaceholder('use %VAULT_NAME% as placeholder')
+				.setValue(this.plugin.settings.bucketPathPrefix)
+				.onChange(async (value) => {
+					this.plugin.settings.bucketPathPrefix = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Test configurations')
+			.setDesc('Will check if credentials are valid and S3 bucket exist')
+			.addButton(button => button
+				.setButtonText("Test")
+				.onClick(async () => {
+					await this.plugin.runTest()
+				}));
+
+		new Setting(containerEl)
+			.setName('Local file protection')
+			.setDesc('Protect local files from deletion')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.localFileProtection)
+				.onChange(async (value) => {
+					this.plugin.settings.localFileProtection = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Synch direction')
+			.setDesc('Non existing files in destination will be deleted if not found in source')
+			.addDropdown(dropdown => dropdown
+        .addOption(SyncDirection.FROM_LOCAL.toString(), "from local to remote")
+        .addOption(SyncDirection.FROM_REMOTE.toString(), "from remote to local")
+				.setValue(this.plugin.settings.syncDirection.toString())
+				.onChange(async (value) => {
+          this.plugin.settings.syncDirection = parseInt(value)
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Enable status bar')
+			.setDesc('Show the synchronization status application bottom bar')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.enableStatusBar)
+				.onChange(async (value) => {
+					this.plugin.settings.enableStatusBar = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Enable notifications')
+			.setDesc('Show notifications whe synchronization status change')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.enableNotifications)
+				.onChange(async (value) => {
+					this.plugin.settings.enableNotifications = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Enable automatic synchronization')
+			.setDesc('Automatically synchronize changes')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.enableAutoSync)
+				.onChange(async (value) => {
+					this.plugin.settings.enableAutoSync = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Automatic synchronization debounce')
+			.setDesc('Delay the synchronization respect vault changes')
+			.addDropdown(dropdown => dropdown
+				.addOptions({
+					'0': "disabled",
+					'2': "2 seconds",
+					'5': "5 seconds",
+					'10': "10 seconds",
+					'30': "30 seconds",
+					'60': "1 minute",
+					'300': "5 minutes",
+				})
+				.setValue(this.plugin.settings.autoSyncDebounce.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.autoSyncDebounce = parseInt(value);
+					await this.plugin.saveSettings();
+				}));
+
+    new Setting(containerEl)
+			.setName('Enable automatic pull')
+			.setDesc('Automatically pull changes from S3 bucket')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.enableAutoPull)
+				.onChange(async (value) => {
+					this.plugin.settings.enableAutoPull = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Automatic pull interval')
+			.setDesc('Interval between S3 bucket changes checks')
+			.addDropdown(dropdown => dropdown
+				.addOptions({
+					'10': "10 seconds",
+					'60': "1 minute",
+					'300': "5 minutes",
+					'600': "10 minutes",
+					'1800': "30 minutes",
+				})
+				.setValue(this.plugin.settings.autoPullInterval.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.autoPullInterval = parseInt(value);
+					await this.plugin.saveSettings();
+				}));
+
+	}
+}
